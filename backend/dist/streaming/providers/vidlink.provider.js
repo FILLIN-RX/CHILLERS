c930698ab6ec98344efe0ae515cfb4cb1a873331
FILLIN-RX <djeutchouruxel@gmail.com>
2026-07-13 16:27:12 +0100

    feat: add VidLink streaming provider implementation

--- backend/dist/streaming/providers/vidlink.provider.js
+++ backend/dist/streaming/providers/vidlink.provider.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VidLinkProvider = void 0;
-const BASE_URL = 'https://vidlink.pro/embed';
+const BASE_URL = 'https://vidlink.pro';
+const PARAMS = 'primaryColor=D70466&autoplay=false';
 class VidLinkProvider {
     constructor() {
         this.name = 'vidlink';
@@ -10,11 +11,11 @@ class VidLinkProvider {
         return true;
     }
     async getMovieStream(query) {
-        const embedUrl = `${BASE_URL}/movie/${query.tmdbId}`;
+        const embedUrl = `${BASE_URL}/movie/${query.tmdbId}?${PARAMS}`;
         return { provider: this.name, embedUrl, type: 'movie' };
     }
     async getEpisodeStream(query) {
-        const embedUrl = `${BASE_URL}/tv/${query.tmdbId}/${query.season || 1}/${query.episode || 1}`;
+        const embedUrl = `${BASE_URL}/tv/${query.tmdbId}/${query.season || 1}/${query.episode || 1}?${PARAMS}`;
         return { provider: this.name, embedUrl, type: 'episode' };
     }
 }
--- backend/src/streaming/providers/vidlink.provider.ts
+++ backend/src/streaming/providers/vidlink.provider.ts
@@ -1,6 +1,7 @@
 import { StreamingProvider, StreamResult, StreamQuery } from './provider.interface';
 
-const BASE_URL = 'https://vidlink.pro/embed';
+const BASE_URL = 'https://vidlink.pro';
+const PARAMS = 'primaryColor=D70466&autoplay=false';
 
 export class VidLinkProvider implements StreamingProvider {
   readonly name = 'vidlink';
@@ -10,12 +11,12 @@ export class VidLinkProvider implements StreamingProvider {
   }
 
   async getMovieStream(query: StreamQuery): Promise<StreamResult | null> {
-    const embedUrl = `${BASE_URL}/movie/${query.tmdbId}`;
+    const embedUrl = `${BASE_URL}/movie/${query.tmdbId}?${PARAMS}`;
     return { provider: this.name, embedUrl, type: 'movie' };
   }
 
   async getEpisodeStream(query: StreamQuery): Promise<StreamResult | null> {
-    const embedUrl = `${BASE_URL}/tv/${query.tmdbId}/${query.season || 1}/${query.episode || 1}`;
+    const embedUrl = `${BASE_URL}/tv/${query.tmdbId}/${query.season || 1}/${query.episode || 1}?${PARAMS}`;
     return { provider: this.name, embedUrl, type: 'episode' };
   }
 }
